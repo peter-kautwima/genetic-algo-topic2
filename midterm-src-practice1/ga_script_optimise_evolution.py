964fbf3ef22644739b1544e7e8240165ee9f3b1c
Peter Kautwima <33910776+peter-kautwima@users.noreply.github.com>
2024-01-06 03:59:45 +0200

    update the ga_script

--- midterm-src-practice1/ga_script_optimise_evolution.py
+++ midterm-src-practice1/ga_script_optimise_evolution.py
@@ -1,17 +1,23 @@
 import os
 import glob
 import subprocess
+from offline_csv_my_version import calculate_distances, find_best_csv
 
-def main(ga_output_path, best_dna_path):
+
+def main():
     # Run the genetic algorithm
     subprocess.run(["python", "test_ga_no_threads.py"], check=True)
 
-    # Select the best DNA
+    # Process all CSV files with offline_csv_my_version.py
     for csv_file in glob.glob('*.csv'):
         subprocess.run(["python", "offline_csv_my_version.py", csv_file], check=True)
 
+    # Calculate the distances and find the best CSV file
+    distances = calculate_distances()
+    best_csv_file = find_best_csv(distances)
+
     # Simulate the best creature
-    subprocess.run(["python", "realtime_from_csv.py", best_dna_path], check=True)
+    subprocess.run(["python", "realtime_from_csv.py", best_csv_file], check=True)
 
 if __name__ == "__main__":
-    main('ga_output.csv', 'best_dna.csv')
+    main()
