144dc98937084303066ca25cb29d2044aa22352b
Peter Kautwima <33910776+peter-kautwima@users.noreply.github.com>
2024-01-07 21:32:51 +0200

    fix script

--- midterm-src-practice1/ga_script_optimise_evolution.py
+++ midterm-src-practice1/ga_script_optimise_evolution.py
@@ -1,24 +1,22 @@
 import os
 import glob
 import subprocess
-from offline_csv_my_version import calculate_distances, find_best_csv
+# from offline_csv_my_version import calculate_distances, find_best_csv
 
 
 def main():
     # Run the genetic algorithm
     subprocess.run(["python", "test_ga_no_threads_keeps_value_elites.py"], check=True)
 
-    # Process all CSV files with offline_csv_my_version.py
     for csv_file in glob.glob('*.csv'):
+        if csv_file == 'ga_output.csv':
+            continue  # Ignore ga_output.csv file
         subprocess.run(["python", "offline_csv_my_version.py", csv_file], check=True)
 
-    # Calculate the distances and find the best CSV file
-    distances = calculate_distances()
-    best_csv_file = find_best_csv(distances)
-
-    # Simulate the best creature
-    subprocess.run(["python", "realtime_from_csv_my_version.py", best_csv_file], check=True)
-    return best_csv_file
+    for csv_file in glob.glob('*.csv'):
+        if csv_file == 'ga_output.csv':
+            continue  # Ignore ga_output.csv file
+        subprocess.run(["python", "realtime_from_csv_my_version.py", csv_file], check=True)
 
 if __name__ == "__main__":
     print(main())
